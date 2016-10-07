30d5b2d6c97307b4e1cb694445f8737ee90ea536
trung <trinhquoc_trung@yahoo.com>
2016-10-07 23:31:00 +0700

    refactor some code

--- src/common/DisplayOutput.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-/**
- * Created by trung on 22/09/2016.
- */
-public interface DisplayOutput {
-    void show(String output);
-}
--- src/common/ReadInput.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-/**
- * Created by trung on 22/09/2016.
- */
-public interface ReadInput {
-    String read();
-}
